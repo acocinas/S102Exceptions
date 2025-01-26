8d13d818d239c3cba642c31df9ba80ae3c2f5b94
acocinas <alcogar79@gmail.com>
2025-01-26 14:00:26 +0100

    main del nivel 2

---
+++ src/Nivel2Ejercicio1/Main/Main.java
@@ -0,0 +1,25 @@
+package Nivel2Ejercicio1.Main;
+
+import static Nivel2Ejercicio1.Model.InputClass.*;
+
+public class Main {
+    public static void main(String[] args) {
+        byte age = readByte("Introduce tu edad:");
+        int number = readInt("Introduce un número entero:");
+        float decimalNumber = readFloat("Introduce un número decimal:");
+        double bigNumber = readDouble("Introduce un número grande:");
+        char character = readChar("Introduce una letra:");
+        String text = readString("Introduce una palabra:");
+        boolean answer = readSiNo("¿Te gusta la programación?");
+
+        System.out.println("Datos introducidos:");
+        System.out.println("Edad: " + age);
+        System.out.println("Número entero: " + number);
+        System.out.println("Número decimal: " + decimalNumber);
+        System.out.println("Número grande: " + bigNumber);
+        System.out.println("Letra: " + character);
+        System.out.println("Texto: " + text);
+        System.out.println("¿Te gusta la programación?: " + (answer ? "Sí" : "No"));
+       
+    }
+}
